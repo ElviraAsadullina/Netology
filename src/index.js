f10e83e0e49b779d805e9b631f022bd0d3adbbb8
Elvira <eliasufa@yahoo.com>
2023-05-24 18:23:09 +0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ let username = "my name";
 let bonusBalance = 1000;
 let bonusPlus = 50;
 let bonusMinus = 3;
-let finalBonusCount = bonusBalance + (bonusPlus * 3) - (bonusMinus * 7);
+let daysCount = 7;
+let step = 2
+let finalBonusCount = bonusBalance + (Math.floor(daysCount / step) * bonusPlus) - (bonusMinus * daysCount);
 console.log(`Пользователь ${username}`);
 console.log(`Баланс ${bonusBalance}`);
 console.log(`Баланс на конец недели: ${finalBonusCount}`);
